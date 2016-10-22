c8d57f29e0d5b707768ec097251cbfbd65bcbe6c
Hanif Fakira <fakira.work@gmail.com>
2016-10-22 09:33:08 +0100

    refactor fizzbuzz instantiation

--- test/FizzbuzzTest.java
+++ test/FizzbuzzTest.java
@@ -1,14 +1,20 @@
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzbuzzTest {
+	private Fizzbuzz fizzbuzz;
+
+	@Before
+	public void setUp() throws Exception {
+		fizzbuzz = 	new Fizzbuzz();
+	}
+	
 	@Test
 	public void canCreateClass() {
-		new Fizzbuzz();
 	}
 	
 	@Test
 	public void canEvaluate() {
-		Fizzbuzz fizzbuzz = 	new Fizzbuzz();
 		fizzbuzz.evaluate(1);
 	}
 }
